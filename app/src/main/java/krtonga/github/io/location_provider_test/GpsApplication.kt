890775989ad4f69e305f852e8e15fd18a23d95de
krtonga <kristen.tonga@gmail.com>
2018-02-28 16:00:20 -0500

    Add Crashalytics

--- app/src/main/java/krtonga/github/io/location_provider_test/GpsApplication.kt
+++ app/src/main/java/krtonga/github/io/location_provider_test/GpsApplication.kt
@@ -1,8 +1,12 @@
 package krtonga.github.io.location_provider_test
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.mapbox.mapboxsdk.Mapbox
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
+
+
 
 class GpsApplication : Application() {
 
@@ -13,5 +17,7 @@ class GpsApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
         Mapbox.getInstance(applicationContext, getString(R.string.mapbox_token))
+
+        Fabric.with(this, Crashlytics())
     }
 }
